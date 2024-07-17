d3a6a1e9627c64405f245cbc0fdb60e685cf58df
Jonas Braun <jonas@iekadou.com>
2024-07-17 10:49:27 +0200

    Add Syntax Check to Slovakia (#45)
    * Add Syntax Check to Slovakia
    
    * Fix codestyle
    
    * Refactor validRule of Slovakia
    
    ---------
    
    Co-authored-by: Jonas Braun <jonas.braun@c24.de>

--- src/CountryHandler/Slovakia.php
+++ src/CountryHandler/Slovakia.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 
 namespace loophp\Tin\CountryHandler;
 
+use function strlen;
+
 /**
  * Slovakia.
  */
@@ -24,12 +26,27 @@ final class Slovakia extends CountryHandler
      */
     public const LENGTH = 10;
 
+    public const PATTERN = '([1-9]\\d[234789]\\d{7})|(\\d{2}[0156]\\d[0-3]\\d{4,5})';
+
+    public function hasValidRule(string $tin): bool
+    {
+        if (strlen($tin) === 10) {
+            if ((int) $tin % 11 === 0) {
+                return true;
+            }
+
+            return (int) substr($tin, 9, 1) === ((int) substr($tin, 0, 9) % 11) % 10;
+        }
+
+        return true;
+    }
+
     protected function hasValidLength(string $tin): bool
     {
         $c1c2 = substr($tin, 0, 2);
 
         if (54 > $c1c2) {
-            return $this->matchLength($tin, self::LENGTH - 1);
+            return $this->matchLength($tin, self::LENGTH) || $this->matchLength($tin, self::LENGTH - 1);
         }
 
         return parent::hasValidLength($tin);
